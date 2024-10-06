25b256ab2340e6060339cc4effa8837b721ca47a
sharmadevanand99@gmail.com <sharmadevanand99@gmail.com>
2024-10-06 16:32:23 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <AppWrapper>
-      <h1>Image Compressor (by Deva)</h1>
+      <h1>Image Compressor <br />(by Deva)</h1>
       <ImageUploader />
     </AppWrapper>
   );
